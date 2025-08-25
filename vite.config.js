19a87b44cd75e14088a05d1f7f61b89773522567
suphakorn <suphakorn.mo@inet.co.th>
2025-08-25 20:19:13 +0700

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+  build: {
+    outDir: 'dist', // โฟลเดอร์ build
   }
 })
